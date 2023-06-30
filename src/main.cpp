8bcf00ca61a66773639ca107183ba98dda8dcc16
roghed <janrogpl@gmail.com>
2023-06-30 17:50:51 +0200

    Rewrite window creation to use SFML instead of GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
-#include "glfw_instance.hpp"
-#include "glfw_window.hpp"
-#include <iostream>
-#include <thread>
+#include <SFML/Window.hpp>
+#include <stdexcept>
 
 int main()
 {
-    GLFWInstance glfw_instance;
-    auto window = glfw_instance.createWindow(800, 600, "Convergence visualizer");
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+    sf::Window main_window(sf::VideoMode(800, 600), "Convergence visualizer");
+    sf::Event event;
+    main_window.display();
+
+    while (main_window.isOpen())
+    {
+        if (!main_window.waitEvent(event))
+        {
+            throw std::runtime_error("Unable to poll window events");
+        }
+
+        if (event.type == sf::Event::Closed)
+        {
+            main_window.close();
+        }
+    }
 }
