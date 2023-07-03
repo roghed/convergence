5c734aeb5e8cbfb2637c9329630f7ac6b39c0408
roghed <janrogpl@gmail.com>
2023-07-03 02:00:18 +0200

    Add grid labels
    For now displaying placeholder strings

--- src/application_window.cpp
+++ src/application_window.cpp
@@ -2,14 +2,18 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window.hpp>
 #include <cmath>
+#include <vector>
 
 ApplicationWindow::ApplicationWindow(int width, int height)
     :
     sf::RenderWindow(sf::VideoMode(width, height), "Convergence visualizer"),
     viewCenter_(2.5, 0),
-    viewSize_(10.0, 5.0)
+    viewSize_(10.0, 5.0),
+    gridScale_(1.0)
 {
     loadShaders();
+    loadLabelsFont();
+    recalculateGrid();
     setShaderInputs();
     rerender();
 
@@ -43,6 +47,7 @@ ApplicationWindow::ApplicationWindow(int width, int height)
 
                     viewSize_ *= std::pow(ZOOM_FACTOR, e.mouseWheelScroll.delta);
 
+                    recalculateGrid();
                     setShaderInputs();
                     rerender();
                 }
@@ -60,6 +65,7 @@ ApplicationWindow::ApplicationWindow(int width, int height)
 
             viewCenter_ -= calc_space_delta;
 
+            recalculateGrid();
             setShaderInputs();
             rerender();
         }
@@ -74,25 +80,83 @@ void ApplicationWindow::loadShaders()
     }
 }
 
+void ApplicationWindow::loadLabelsFont()
+{
+    // TODO: make font loading portable on every system
+    labelsFont_.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-R.ttf");
+}
+
 void ApplicationWindow::setShaderInputs()
 {
     sf::Vector2f screen_size = static_cast<sf::Vector2f>(sf::RenderWindow::getSize());
     renderArea_ = sf::RectangleShape(screen_size);
-    float grid_scale = std::pow(10.0, std::ceil(std::log10(viewSize_.x * 5)) - 2);
 
     iterateShader_.setUniform("screenSize", screen_size);
     iterateShader_.setUniform("viewCenter", viewCenter_);
     iterateShader_.setUniform("viewSize", viewSize_);
-    iterateShader_.setUniform("gridScale", grid_scale);
+    iterateShader_.setUniform("gridScale", gridScale_);
+}
+
+void ApplicationWindow::recalculateGrid()
+{
+    gridScale_ = std::pow(10.0, std::ceil(std::log10(viewSize_.x * 5)) - 2);
+    sf::Vector2f view_bot_left = viewCenter_ - viewSize_ * 0.5f;
+
+    xAxisLabels_.resize((int)(viewSize_.x / gridScale_) + 2);
+    for (int i = 0; i < xAxisLabels_.size(); ++i)
+    {
+        auto& label = xAxisLabels_[i];
+
+        label.setFont(labelsFont_);
+        label.setCharacterSize(16);
+        label.setString("xxx"); // TODO: set to numeric value of the label
+        label.setFillColor(sf::Color::Blue);
+        label.setOrigin(0, 2 * label.getLocalBounds().height);
+
+        sf::Vector2f pos;
+        pos.x = gridScale_ * (std::ceil(view_bot_left.x / gridScale_) + i - 1);
+        pos.y = view_bot_left.y;
+        sf::Vector2i window_space_pos = toWindowSpaceCoordinates(pos);
+        label.setPosition((sf::Vector2f)(window_space_pos));
+    }
+
+    yAxisLabels_.resize((int)(viewSize_.y / gridScale_) + 2);
+    for (int i = 0; i < yAxisLabels_.size(); ++i)
+    {
+        auto& label = yAxisLabels_[i];
+
+        label.setFont(labelsFont_);
+        label.setCharacterSize(16);
+        label.setString("yyy"); // TODO: set to numeric value of the label
+        label.setFillColor(sf::Color::Blue);
+        label.setOrigin(0, 2 * label.getLocalBounds().height);
+
+        sf::Vector2f pos;
+        pos.x = view_bot_left.x;
+        pos.y = gridScale_ * (std::ceil(view_bot_left.y / gridScale_) + i - 1);
+        sf::Vector2i window_space_pos = toWindowSpaceCoordinates(pos);
+        label.setPosition((sf::Vector2f)(window_space_pos));
+    }
 }
 
 void ApplicationWindow::rerender()
 {
     sf::RenderWindow::draw(renderArea_, &iterateShader_);
+
+    for (const auto& label : xAxisLabels_)
+    {
+        sf::RenderWindow::draw(label);
+    }
+
+    for (const auto& label : yAxisLabels_)
+    {
+        sf::RenderWindow::draw(label);
+    }
+
     sf::RenderWindow::display();
 }
 
-sf::Vector2f ApplicationWindow::toCalcSpaceCoordinates(sf::Vector2i window_coord)
+sf::Vector2f ApplicationWindow::toCalcSpaceCoordinates(sf::Vector2i window_coord) const
 {
     auto screen_size = sf::Vector2f(ApplicationWindow::getSize());
     auto normalized_pixel_coord =
@@ -102,3 +166,22 @@ sf::Vector2f ApplicationWindow::toCalcSpaceCoordinates(sf::Vector2i window_coord
     return sf::Vector2f(x, y);
 }
 
+sf::Vector2i ApplicationWindow::toWindowSpaceCoordinates(sf::Vector2f calc_space_coord) const
+{
+    auto screen_size = sf::Vector2f(ApplicationWindow::getSize());
+    auto bot_left_corner = viewCenter_ - 0.5f * viewSize_;
+
+    sf::Vector2f result =
+    {
+        (calc_space_coord.x - bot_left_corner.x) / viewSize_.x * screen_size.x,
+        (calc_space_coord.y - bot_left_corner.y) / viewSize_.y * screen_size.y
+    };
+
+    result = sf::Vector2f
+    {
+        ((calc_space_coord.x - viewCenter_.x) / viewSize_.x + 0.5f) * screen_size.x,
+        ((calc_space_coord.y - viewCenter_.y) / viewSize_.y + 0.5f) * screen_size.y
+    };
+
+    return {(int)result.x, (int)(screen_size.y - result.y)};
+}
--- src/application_window.hpp
+++ src/application_window.hpp
@@ -3,6 +3,8 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/Shader.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <vector>
 
 class ApplicationWindow : protected sf::RenderWindow
 {
@@ -15,14 +17,21 @@ public:
 private:
 
     void loadShaders();
+    void loadLabelsFont();
+    void recalculateGrid();
     void setShaderInputs();
     void rerender();
-    sf::Vector2f toCalcSpaceCoordinates(sf::Vector2i window_coord);
+    sf::Vector2f toCalcSpaceCoordinates(sf::Vector2i window_coord) const;
+    sf::Vector2i toWindowSpaceCoordinates(sf::Vector2f calc_space_coord) const;
 
-    MouseDrag          mouseDrag_;
-    sf::RectangleShape renderArea_;
-    sf::Shader         iterateShader_;
-    sf::Vector2f       viewCenter_;
-    sf::Vector2f       viewSize_;
+    MouseDrag             mouseDrag_;
+    sf::RectangleShape    renderArea_;
+    sf::Shader            iterateShader_;
+    sf::Vector2f          viewCenter_;
+    sf::Vector2f          viewSize_;
+    float                 gridScale_;
+    sf::Font              labelsFont_;
+    std::vector<sf::Text> xAxisLabels_;
+    std::vector<sf::Text> yAxisLabels_;
 };
 
